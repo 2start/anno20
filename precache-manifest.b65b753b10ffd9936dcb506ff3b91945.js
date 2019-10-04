9ba99021787da8b5c265cd2964a00ab8d64bbd85
= <nduldhardt@gmail.com>
2019-10-05 00:34:21 +0200

    Updates

---
+++ precache-manifest.b65b753b10ffd9936dcb506ff3b91945.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba7b5e41449eeb5e9902d1bf7aead4d8",
+    "url": "/anno20/index.html"
+  },
+  {
+    "revision": "9eed946ae43d3d81a977",
+    "url": "/anno20/static/js/2.f1a75794.chunk.js"
+  },
+  {
+    "revision": "49d956fa4764b45029c7",
+    "url": "/anno20/static/js/main.6f3c8fd7.chunk.js"
+  },
+  {
+    "revision": "621f0f352e88d2aad746",
+    "url": "/anno20/static/js/runtime-main.cdd451d0.js"
+  }
+]);
